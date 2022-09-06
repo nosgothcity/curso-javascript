0623e6f16530d39ceda23949d6b3fda1cbcebf6c
nosgothcity <fer.diaz.crichton@gmail.com>
2022-09-05 20:54:02 -0400

    Segunda entrega

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const NUMERO_A_MULTIPLICAR = parseInt(prompt('Ingrese un número entero para multiplicar'))
-const LIMITE = parseInt(prompt('Ingrese un multiplicador que será el limite'))
-let multiplicador = 1;
+const PRIMER_NUMERO = parseInt(prompt('Ingrese un número entero'))
+const SEGUNDO_NUMERO = parseInt(prompt('Ingrese otro número entero'))
 
-console.log('El número a multiplicar es:' + NUMERO_A_MULTIPLICAR + ' y su multiplicador limite es el: ' + LIMITE)
-
-while(true){
-    const RESULTADO = NUMERO_A_MULTIPLICAR * multiplicador
-    console.log(RESULTADO)
-    multiplicador++
-    if(multiplicador > LIMITE){
-        break;
+const checkOperationType = () => {
+    let flag = true;
+    let operacion = ''
+    while(flag){
+        const TIPO_OPERACION = prompt('Ingrese tipo de operación que desea realizar con los números ingresados (+, -, *, /)').trim()
+        if(TIPO_OPERACION === '+' || TIPO_OPERACION === '-' || TIPO_OPERACION === '*' || TIPO_OPERACION === '/'){
+            operacion = TIPO_OPERACION
+            flag = false
+        } else {
+            alert('Ingrese una operación válida a realizar (+, -, *, /).')
+        }
     }
-}
+    return operacion
+};
+
+const resolveOperation = (PRIMER_NUMERO, SEGUNDO_NUMERO, OPERACION) => {
+    let resultado = 0
+    switch (OPERACION) {
+        case '+':
+            resultado = PRIMER_NUMERO + SEGUNDO_NUMERO
+            break;
+        case '-':
+            resultado = PRIMER_NUMERO - SEGUNDO_NUMERO
+            break;
+        case '*':
+            resultado = PRIMER_NUMERO * SEGUNDO_NUMERO
+            break;
+        case '/':
+            resultado = PRIMER_NUMERO / SEGUNDO_NUMERO
+            break;
+        default:
+            break;
+    }
+
+    return resultado;
+};
+
+const OPERACION = checkOperationType()
+const RESULTADO_OPERACION = resolveOperation(PRIMER_NUMERO, SEGUNDO_NUMERO, OPERACION)
+
+alert('El resultado de ' + PRIMER_NUMERO + OPERACION + SEGUNDO_NUMERO + ' es: ' + RESULTADO_OPERACION)
